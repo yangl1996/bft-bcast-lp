1e4463e31c35301349bed3b0760010235bcb5650
Lei Yang <lei@yangl1996.com>
2022-12-12 16:37:42 -0500

    init

---
+++ problem.py
@@ -0,0 +1,50 @@
+import pulp as pl
+
+# the first node is the leader
+ingress = [10.0, 10.0, 10.0, 10.0]
+egress = [10.0, 10.0, 10.0, 10.0]
+
+if len(ingress) != len(egress):
+    print("ingress and egress constraints do not match")
+    exit(1)
+
+N = len(ingress)
+
+nodes = [i for i in range(N)]
+dests = [i for i in range(N) if i != 0]
+
+# rate on each edge in flows from leader to each follower
+flowEdgeVars = pl.LpVariable.dicts("FlowEdge", (dests, nodes, nodes), lowBound=0)
+minMaxFlowVar = pl.LpVariable("MinMaxFlow", lowBound=0)
+
+prob = pl.LpProblem("BftBcast", pl.LpMaximize)
+
+# objective
+prob += (minMaxFlowVar, "NCThroughput")
+
+# no loopback
+for d in dests:
+    for n in nodes:
+        prob += (flowEdgeVars[d][n][n] == 0, f"NoLoopbackFlow{d}Node{n}")
+
+# flow conservation
+for d in dests:
+    for n in nodes:
+        if n != 0 and n != d:
+            prob += (pl.lpSum([flowEdgeVars[d][n][i] for i in nodes]) - pl.lpSum([flowEdgeVars[d][i][n] for i in nodes]) == 0, f"FlowConservationFlow{d}Node{n}")
+
+# egress caps
+for d in dests:
+    for n in nodes:
+        prob += (pl.lpSum([flowEdgeVars[d][n][i] for i in nodes]) <= egress[n], f"EgressCapFlow{d}Node{n}")
+
+# ingress caps
+for d in dests:
+    for n in nodes:
+        prob += (pl.lpSum([flowEdgeVars[d][i][n] for i in nodes]) <= ingress[n], f"IngressCapFlow{d}Node{n}")
+
+# definition of minMaxFlow
+for d in dests:
+    prob += (pl.lpSum([flowEdgeVars[d][0][i] for i in nodes]) - minMaxFlowVar >= 0, f"MinMaxNoGreaterThanFlow{d}")
+
+prob.writeMPS("throughput.mps")
