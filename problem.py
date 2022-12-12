361aae7f4744b0942b44fe22fd45cfc1e5d34a33
Lei Yang <lei@yangl1996.com>
2022-12-12 17:24:20 -0500

    decide per-edge cap first

--- problem.py
+++ problem.py
@@ -15,6 +15,9 @@ dests = [i for i in range(N) if i != 0]
 
 # rate on each edge in flows from leader to each follower
 flowEdgeVars = pl.LpVariable.dicts("FlowEdge", (dests, nodes, nodes), lowBound=0)
+# edge cap satisfying node cap
+edgeCapVars = pl.LpVariable.dicts("EdgeCap", (nodes, nodes), lowBound=0)
+# min over max flows from leader to each follower
 minMaxFlowVar = pl.LpVariable("MinMaxFlow", lowBound=0)
 
 prob = pl.LpProblem("BftBcast", pl.LpMaximize)
@@ -26,6 +29,16 @@ prob += (minMaxFlowVar, "NCThroughput")
 for d in dests:
     for n in nodes:
         prob += (flowEdgeVars[d][n][n] == 0, f"NoLoopbackFlow{d}Node{n}")
+for n in nodes:
+    prob += (edgeCapVars[n][n] == 0, f"NoLoopbackCapNode{n}")
+
+# egress caps
+for n in nodes:
+    prob += (pl.lpSum([edgeCapVars[n][i] for i in nodes]) <= egress[n], f"EgressCapNode{n}")
+
+# ingress caps
+for n in nodes:
+    prob += (pl.lpSum([edgeCapVars[i][n] for i in nodes]) <= ingress[n], f"IngressCapNode{n}")
 
 # flow conservation
 for d in dests:
@@ -33,15 +46,11 @@ for d in dests:
         if n != 0 and n != d:
             prob += (pl.lpSum([flowEdgeVars[d][n][i] for i in nodes]) - pl.lpSum([flowEdgeVars[d][i][n] for i in nodes]) == 0, f"FlowConservationFlow{d}Node{n}")
 
-# egress caps
-for d in dests:
-    for n in nodes:
-        prob += (pl.lpSum([flowEdgeVars[d][n][i] for i in nodes]) <= egress[n], f"EgressCapFlow{d}Node{n}")
-
-# ingress caps
-for d in dests:
-    for n in nodes:
-        prob += (pl.lpSum([flowEdgeVars[d][i][n] for i in nodes]) <= ingress[n], f"IngressCapFlow{d}Node{n}")
+# edge capacity
+for n in nodes:
+    for m in nodes:
+        for d in dests:
+            prob += (edgeCapVars[n][m] - flowEdgeVars[d][n][m] >= 0, f"EdgeCapFlow{d}From{n}To{m}")
 
 # definition of minMaxFlow
 for d in dests:
